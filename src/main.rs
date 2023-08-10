4469c7b28a65dedb3759f1163685288b54d57fb1
Branislav Hozza <brankohozza@gmail.com>
2023-08-10 16:16:20 +0200

    :sparkles: add exit game func

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
-use bevy::prelude::*;
+use bevy::{app::AppExit, prelude::*};
 
 mod plugins;
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, plugins::game::GamePlugin))
+        .add_systems(Update, exit_game)
         .run();
+}
+
+pub fn exit_game(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut app_exit_event_writer: EventWriter<AppExit>,
+) {
+    if keyboard_input.just_pressed(KeyCode::Escape) {
+        app_exit_event_writer.send(AppExit);
+    }
 }
